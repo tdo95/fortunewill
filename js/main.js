f8d1ddbcedf33e27ef7c7729c8c8223e81a7c282
Tee O <tdopress@gmail.com>
2022-08-28 03:02:01 -0400

    implemented fetch and delay functionalit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
 let fortuneBtn = document.querySelector('.fortune-btn');
 let input = document.querySelector('.question-input');
-let errorTextBox = document.querySelector('.error-box')
+let errorTextBox = document.querySelector('.error-box');
+let crystalBall = document.querySelector('.crystal-ball');
+let entryScreen = document.querySelector('.entry-screen');
+let responseScreen = document.querySelector('.response-screen');
+let questionBox = document.querySelector('.question');
+let answerBox = document.querySelector('.answer');
+let newQuestionBtn = document.querySelector('.new-question');
 
-fortuneBtn.addEventListener('click', processRequest)
+
+newQuestionBtn.addEventListener('click', () => {
+    toggleResponseScreen()
+    loadPastFortunes() 
+});
+
+fortuneBtn.addEventListener('click', async () => processRequest());
 
 async function processRequest() {
+    let question = input.value;
     //validate text
-   let validQuestion = validateQuestion(input.value);
-   if (!validQuestion) return;
+    let validQuestion = validateQuestion(question);
+    if (!validQuestion) return;
+    //get response from server
+    let response = await fetchFortuneResponse();
+    //set fortune response in display
+    setFortuneResponse(question, response);
+    console.log(response);
 
-   //get response from server
-   let response = await fetchFortuneResponse()
-   console.log(response);
+    //TODO: show loading animation
 
-   //show loading animation
+    //TODO: unhide response screen
+    toggleResponseScreen();
 
-   //display fortune
+    //wait 1 seconds
+    await waitFor(1000);
 
-   //save fortune in local storage
+    //TODO: hide loading animation
+
+    //TODO: save fortune in local storage
+    saveFortuneInLocalStorage();
 }
 
 
@@ -37,11 +58,43 @@ function validateQuestion(question) {
         errorTextBox.innerText = 'Please ask a yes or no question';
         return false;
     } else return true;
-
 }
 
+//get fortune response from server
 async function fetchFortuneResponse() {
     let response = await fetch('/api');
     let data = await response.json();
     return data.response;
 }
+
+//set response in display
+function setFortuneResponse(question, answer) {
+    questionBox.innerText = question;
+    answerBox.innerText = answer;
+}
+
+function toggleResponseScreen() {
+    let classList = responseScreen.classList;
+    if (classList.contains('hidden')) classList.remove('hidden');
+    //display transition
+    else {
+        //hide entry screen
+
+        //fade out response screen
+
+        //fade in entry screen
+    }
+}
+
+//load past fortunes into DOM using local storage - loads last 5 responses
+function loadPastFortunes() {
+
+}
+
+//save fortune into local storage storing question and response in an object
+function saveFortuneInLocalStorage() {
+
+
+}
+
+waitFor = async (delay) => new Promise(resolve => setTimeout(resolve, delay));
