67542c9e308cd6d61783424f1ef5adeb23bfb792
Tee O <tdopress@gmail.com>
2022-08-27 20:53:26 -0400

    added fortune responses file

---
+++ reponses.js
@@ -0,0 +1,10 @@
+export const fortuneAnswers = {
+    1: 'Don\'t count on it',
+    2: 'Outlook not so good',
+    3: 'Most likely, the future looks bright',
+    4: 'I can\'t predict that right now. Come back and ask when that stars are clearer',
+    5: 'The stars are in you favor.',
+    6: "I've learned never say never",
+    7: "It's written in the stars",
+
+}
