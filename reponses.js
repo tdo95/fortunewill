3d99dd9fb9b1c268045feec649a66abeba1eadd4
Tee O <tdopress@gmail.com>
2022-08-31 19:48:01 -0400

    added more responses

--- reponses.js
+++ reponses.js
@@ -3,13 +3,16 @@ const fortuneAnswers = {
     1: 'Don\'t count on it.',
     2: 'Outlook not so good.',
     3: 'Most likely, the future looks bright.',
-    4: 'I can\'t predict that right now. Come back and ask when that stars are clearer.',
+    4: 'I can\'t predict that right now. Come back and ask when the stars are clearer.',
     5: 'The stars are in your favor.',
     6: "I've learned never say never.",
-    7: "It's written in the stars.",
-    8: "Unfortunately, it's a no from me.",
+    7: "The stars say yes.",
+    8: "Unfortunately, the gods say no.",
     9: "So many questions, I am worn out! Ask again later.",
-    10: "Absolutely, the skys tell me it is so."
+    10: "Absolutely, the skys tell me it is so.",
+    11: 'Yes.',
+    12: 'No.',
+    13: 'Perhaps.'
 
 }
 
